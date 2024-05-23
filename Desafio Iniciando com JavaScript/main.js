c3b751958b0377c77298b0ca9771f45329a1016b
Augusto Henrique <106348435+Guto-H@users.noreply.github.com>
2024-05-23 16:41:47 -0300

    Add files via upload

---
+++ Desafio Iniciando com JavaScript/main.js
@@ -0,0 +1,99 @@
+
+/*  Exercicio 1   */
+
+/*alert("Hello World!");*/
+
+
+
+
+/*  Exercicio 2   */
+
+/*let numberOne = Number(prompt("Insira o primeiro numero:"));
+let numberTwo = Number(prompt("Insira o segundo numero:"));
+
+alert(`Resultado da soma é: ${numberOne + numberTwo}`);*/
+
+
+
+
+/*  Exercicio 3  */
+
+/*let data = prompt("Insira um valor: ");
+
+if(isNaN(data)){
+    alert("Não é um número");
+}
+else{
+    alert("É um numero");
+}*/
+
+
+
+/*  Exercicio 4 */
+
+/*let data = prompt("Insira um valor: ");
+
+let isAnumber = !isNaN(data) ? "É um número" : "Não é um número";
+
+alert(isAnumber)*/
+
+
+
+
+/*   Exercicio 5 */
+
+/*let data = prompt("Insira um valor: ");
+
+let isString = isNaN(data) ? "É uma string" : "Não é uma string";
+
+alert(isString)*/
+
+
+
+/*   Exercicio 6 */
+
+/*let numberOne = Number(prompt("Insira o primeiro numero:"));
+let numberTwo = Number(prompt("Insira o segundo numero:"));
+
+alert(`Resultado da subtração é: ${numberOne - numberTwo}`);*/
+
+
+/* Exercicio 7 */
+
+/*let numberOne = Number(prompt("Insira o primeiro numero:"));
+let numberTwo = Number(prompt("Insira o segundo numero:"));
+
+alert(`Resultado da multiplicação é: ${numberOne * numberTwo}`);*/
+
+
+
+/* Exercicio 8 */
+
+/*let numberOne = Number(prompt("Insira o primeiro numero:"));
+let numberTwo = Number(prompt("Insira o segundo numero:"));
+
+alert(`Resultado da divisão é: ${numberOne / numberTwo}`);*/
+
+
+
+/* Exercicio 9*/
+
+/*let number = Number(prompt("Insira o numero:"));
+
+let EvenOdd = number % 2 == 0 ? "É um número par" : "Não é um numero par"
+
+alert(EvenOdd)*/
+
+
+
+/* Exercicio 10 */
+
+
+let number = Number(prompt("Insira o numero:"));
+
+let EvenOdd = number % 2 == 1 ? "É um número ímpar" : "Não é um numero ímpar"
+
+alert(EvenOdd)
+
+
+
